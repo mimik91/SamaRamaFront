c220b21d7069c6fdee4c51cdd4845bf35a678ffa
mimik91 <dominiklach@poczta.fm>
2025-04-08 10:37:18 +0200

    bicycle detail but is not working

---
+++ src/app/service-panel/bike-service.model.ts
@@ -0,0 +1,15 @@
+export interface BikeService {
+    id: number;
+    name: string;
+    street?: string;
+    building?: string;
+    flat?: string;
+    postalCode?: string;
+    city?: string;
+    phoneNumber?: string;
+    businessPhone?: string;
+    email?: string;
+    latitude?: number;
+    longitude?: number;
+    description?: string;
+  }
---
+++ src/app/service-records/service-record.service.ts
@@ -0,0 +1,35 @@
+// src/app/service-records/service-record.service.ts
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, catchError, throwError } from 'rxjs';
+import { ServiceRecord } from './service-record.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ServiceRecordService {
+  private apiUrl = 'http://localhost:8080/api/service-records';
+  private http = inject(HttpClient);
+  
+  constructor() {}
+  
+  getBicycleServiceRecords(bicycleId: number): Observable<ServiceRecord[]> {
+    return this.http.get<ServiceRecord[]>(`${this.apiUrl}/bicycle/${bicycleId}`)
+      .pipe(
+        catchError(error => {
+          console.error(`Error fetching service records for bicycle ${bicycleId}:`, error);
+          return throwError(() => error);
+        })
+      );
+  }
+  
+  addServiceRecord(serviceRecord: Omit<ServiceRecord, 'id'>): Observable<any> {
+    return this.http.post(this.apiUrl, serviceRecord)
+      .pipe(
+        catchError(error => {
+          console.error('Error adding service record:', error);
+          return throwError(() => error);
+        })
+      );
+  }
+}

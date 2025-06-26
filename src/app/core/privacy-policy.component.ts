e7e859eeb013cfd9c8ce053181aff389cd3c0b96
mimik91 <dominiklach@poczta.fm>
2025-06-26 18:53:34 +0200

    privacy policy corrections

--- src/app/core/privacy-policy.component.ts
+++ src/app/core/privacy-policy.component.ts
@@ -15,12 +15,12 @@ import { Router } from '@angular/router';
       
       <section class="privacy-content">
         <h2>I. Postanowienia ogólne</h2>
-        <p>Polityka prywatności określa, jak zbierane, przetwarzane i przechowywane są dane osobowe Użytkowników niezbędne do świadczenia usług drogą elektroniczną za pośrednictwem serwisu internetowego [adres strony] (dalej: Serwis).</p>
+        <p>Polityka prywatności określa, jak zbierane, przetwarzane i przechowywane są dane osobowe Użytkowników niezbędne do świadczenia usług drogą elektroniczną za pośrednictwem serwisu internetowego www.cyclopick.pl (dalej: Serwis).</p>
         <p>Serwis zbiera wyłącznie dane osobowe niezbędne do świadczenia i rozwoju usług w nim oferowanych.</p>
         <p>Dane osobowe zbierane za pośrednictwem Serwisu są przetwarzane zgodnie z Rozporządzeniem Parlamentu Europejskiego i Rady (UE) 2016/679 z dnia 27 kwietnia 2016 r. w sprawie ochrony osób fizycznych w związku z przetwarzaniem danych osobowych i w sprawie swobodnego przepływu takich danych oraz uchylenia dyrektywy 95/46/WE (ogólne rozporządzenie o ochronie danych, dalej RODO) oraz ustawą o ochronie danych osobowych z dnia 10 maja 2018 r.</p>
 
         <h2>II. Administrator danych</h2>
-        <p>Administratorem danych osobowych zbieranych poprzez Serwis jest [nazwa osoby prawnej/przedsiębiorcy/osoby fizycznej], adres: [adres siedziby/zamieszkania], KRS: [numer], NIP: [numer], REGON: [numer], adres poczty elektronicznej: [adres e-mail] (dalej: Administrator).</p>
+        <p>Administratorem danych osobowych zbieranych poprzez Serwis jest Krzysztof Podkanowicz, adres: ulanow 64d Kraków, adres poczty elektronicznej: cyclopick{{'@'}}gmail.com (dalej: Administrator).</p>
 
         <h2>III. Cel zbierania danych osobowych</h2>
         <p>Dane osobowe wykorzystywane są w celu:</p>
@@ -58,7 +58,7 @@ import { Router } from '@angular/router';
 
         <h2>VII. Prawa Użytkowników</h2>
         <p>Użytkownik Serwisu ma prawo do: dostępu do treści swoich danych osobowych, ich sprostowania, usunięcia, ograniczenia przetwarzania, przenoszenia, wniesienia sprzeciwu wobec przetwarzania, cofnięcia zgody w każdej chwili (co nie ma wpływu na zgodność z prawem przetwarzania dokonanego w oparciu o zgodę przed jej cofnięciem).</p>
-        <p>Zgłoszenie o wystąpieniu przez Użytkownika z uprawnieniem wynikającym z wymienionych praw należy przesłać na adres [adres e-mail].</p>
+        <p>Zgłoszenie o wystąpieniu przez Użytkownika z uprawnieniem wynikającym z wymienionych praw należy przesłać na adres cyclopick{{'@'}}gmail.com.</p>
         <p>Administrator spełnia lub odmawia spełnienia żądania niezwłocznie – maksymalnie w ciągu miesiąca od jego otrzymania.</p>
         <p>Użytkownik ma prawo złożyć skargę do Prezesa Urzędu Ochrony Danych Osobowych, jeśli uzna, że przetwarzanie narusza jego prawa i wolności (RODO).</p>
 
